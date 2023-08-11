84901c160d6f6cb3e6a84702ef7dd550dcd8b1f8
vanja0034 <vanja.djunovic9@gmail.com>
2023-08-11 20:09:45 +0200

    Dodate api putanje

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::get('orders', [OrderController::class, 'index']);
+Route::get('products', [ProductController::class, 'index']);
+Route::get('orders/{id}', [OrderController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
 });
