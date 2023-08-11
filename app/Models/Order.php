f07174cb95d7c3892d0f2ee2148699816353718b
vanja0034 <vanja.djunovic9@gmail.com>
2023-08-11 20:03:56 +0200

    Kreirani modeli i migracije

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['address', 'email', 'phone', 'name', 'delivered'];
+
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+}
